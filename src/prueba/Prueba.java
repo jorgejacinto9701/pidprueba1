79b547d88eabf2e8a3a8a2a977a28a69d592a3d3
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2020-09-29 17:27:09 -0500

    ssadsds

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -5,15 +5,10 @@ public class Prueba {
 	public static void main(String[] args) {
 
 
-<<<<<<< HEAD
 		int x1 = 0;
 		int y1 = 0;
 		int z1 = 0;
-=======
-		int x2 = 0;
-		int y2 = 0;
-		int z2 = 0;
->>>>>>> branch 'master' of https://github.com/jorgejacinto9701/pidprueba1.git
+
 
 	}
 
