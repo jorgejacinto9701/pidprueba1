c8d4eb3a34509e3ebdaa032b801c3d5f6e0a59c3
jorge jacinto gutarra <jorgejacinto9701@gmail.com>
2020-09-29 18:59:47 -0500

    Update Prueba.java
    Cambio a valor 5

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -5,9 +5,9 @@ public class Prueba {
 	public static void main(String[] args) {
 
 
-		int x1 = 0;
-		int y1 = 0;
-		int z1 = 0;
+		int x5 = 0;
+		int y5 = 0;
+		int z5 = 0;
 		int cebollin = 0;
 		
 
